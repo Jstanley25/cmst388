5332f66a46f6e92a1d75bef03ac0f3666b420c32
John Stanley <jstanley25@student.umgc.edu>
2024-03-20 21:59:05 -0400

    Project 1 update

--- project1/photo-gallery-assignment/js/image-gallery.js
+++ project1/photo-gallery-assignment/js/image-gallery.js
@@ -1,1 +1,51 @@
 // JavaScript Document
+
+document.addEventListener('DOMContentLoaded', function() {
+    // permanently sets thumbnails to the thumbnails images
+    const thumbnails = document.getElementById('thumbnails').getElementsByTagName('img');
+
+    // permanently sets largeImage the img with id largImage
+    const largeImage = document.getElementById('largeImage');
+
+    // permanently sets figCaption
+    const figCaption = document.getElementById('figCaption');
+
+    // sets selectedThumbnail to null
+    let selectedThumbnail = null;
+
+    // loops through each of the thumbnails
+    for (let i = 0; i < thumbnails.length; i++) {
+
+        // Takes all of the thumbnails as an arrary and adds an eventlistener to listen for the mouseover event
+        thumbnails[i].addEventListener('mouseover', function() {
+
+            // this stores the new largeImage src which is the src of the image being moused over
+            const largeSrc = this.getAttribute('src');
+
+            // this stores the new figCaption text which is the alt text of the image being moused over
+            const figCaptionText = this.getAttribute('alt');
+
+            // Sets the new largeImage src to the src of the image we are hovering over
+            largeImage.src = largeSrc;
+
+            // Sets the new figCaption text to the alt text of the image we are hovering over
+            figCaption.innerText = figCaptionText;
+            
+        });
+
+        // adds an onClick listener to each of the thumbnail images
+        thumbnails[i].addEventListener('click', function() {
+
+            // Checks which image is the selectedThumbnail
+            if (selectedThumbnail) {
+
+                // removes image as selected thumbnail
+                selectedThumbnail.classList.remove('selected');
+            }
+
+            // sets image we just clicked to selectedThumbnail
+            this.classList.add('selected');
+            selectedThumbnail = this;
+        });
+    }
+});
